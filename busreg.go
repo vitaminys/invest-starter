7639a3c24a275e7e4eb6b4c2ea9c7a5800eb6dab
Andrey <vasenin.av@phystech.edu>
2016-05-21 08:02:31 +0300

    Business part

--- busreg.go
+++ busreg.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/binary"
 	"encoding/json"
+	"fmt"
 	"github.com/boltdb/bolt"
 	"github.com/gin-gonic/gin"
 	"log"
@@ -23,18 +24,19 @@ type Business struct {
 }
 
 type BusinessForm struct {
-	Name        string `form:"name" binding:"required"`
-	Type        string `form:"type" binding:"required"`
-	Industry    string `form:"industry" binding:"required"`
-	Description string `form:"description" binding:"required"`
-	City        string `form:"city" binding:"required"`
-	Email       string `form:"email" binding:"required"`
-	Password    string `form:"password" binding:"required"`
+	Name        string //`form:"name" binding:"required"`
+	Type        string //`form:"type" binding:"required"`
+	Industry    string //`form:"industry" binding:"required"`
+	Description string //`form:"description" binding:"required"`
+	City        string //`form:"city" binding:"required"`
+	Email       string //`form:"email" binding:"required"`
+	Password    string //`form:"password" binding:"required"`
 }
 
 func showbusregpage(c *gin.Context) {
-	c.HTML(http.StatusOK, "contact.html", gin.H{
+	c.HTML(http.StatusOK, "register_business.html", gin.H{
 		"title": "Регистрация бизнеса",
+		"name":  siteName,
 	})
 }
 
@@ -42,15 +44,25 @@ func regbusiness(c *gin.Context) {
 	var form BusinessForm
 	bsn := Business{}
 	if c.Bind(&form) == nil {
+		log.Println("register_business")
 		db, err := bolt.Open("my.db", 0600, &bolt.Options{Timeout: 1 * time.Second})
 		if err != nil {
 			log.Fatal(err)
 		}
 		defer db.Close()
 		err = db.Batch(func(tx *bolt.Tx) error {
-			b := tx.Bucket([]byte("business"))
-			id, _ := b.NextSequence()
-			bsn.Id = int(id)
+			b, err := tx.CreateBucketIfNotExists([]byte("MyBucket"))
+			if err != nil {
+				return fmt.Errorf("create bucket: %s", err)
+			}
+			// b := tx.Bucket([]byte("business"))
+			id, err := b.NextSequence()
+			if err != nil {
+				log.Println(err)
+				bsn.Id = 0
+			} else {
+				bsn.Id = int(id)
+			}
 			buf, err := json.Marshal(bsn)
 			if err != nil {
 				return err
@@ -63,15 +75,23 @@ func regbusiness(c *gin.Context) {
 		}
 
 		// if form.User == "user" && form.Password == "password" {
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"title": "Успех. Верификация",
-		})
+		// c.HTML(http.StatusOK, "index.html", gin.H{
+		// 	"title": "Успех. Верификация",
+		// })
 		// } else {
 		// 	c.HTML(http.StatusOK, "index.html", gin.H{
 		// 		"title": "Неуспех",
 		// 	})
 		// }
+		c.Redirect(http.StatusMovedPermanently, "/business-profile")
 	}
+}
+
+func busprofile(c *gin.Context) {
+	c.HTML(http.StatusOK, "profile_business.html", gin.H{
+		"title": "Найстройки профиля",
+		"name":  siteName,
+	})
 }
 
 func itob(v int) []byte {
--- main.go
+++ main.go
@@ -2,31 +2,24 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	// "github.com/gin-gonic/contrib/static"
 	"net/http"
-	// "log"
 )
 
+var siteName string = "InvestStarter"
+
 func main() {
-	// Creates a gin router with default middleware:
-	// logger and recovery (crash-free) middleware
 
 	router := gin.Default()
 
-	// router.StaticFS("files/site/", http.Dir("./"))
 	router.Static("css/", "./files/site/css/")
 	router.Static("js/", "./files/site/js/")
 	router.Static("img/", "./files/site/img/")
-	// router.Static("/img", "./files/site/img/")
-	// router.LoadHTMLGlob("files/site/**/*")
 	router.LoadHTMLGlob("files/site/*.html")
-	// router.Use(static.Serve("/", static.LocalFile("/files/site", false)))
-	// router.NotFound(static.Serve("/", "/files/site"))
-	// router.Use(static.Serve("/", static.LocalFile("/files/site", false)))
 
 	router.GET("/", mpage)
-	router.GET("/business_reg", showbusregpage)
-	router.POST("/business_reg/check", regbusiness)
+	router.GET("/business-reg", showbusregpage)
+	router.POST("/business-reg/check", regbusiness)
+	router.GET("/business-profile", busprofile)
 
 	// By default it serves on :8080 unless a
 	// PORT environment variable was defined.
@@ -42,6 +35,6 @@ func main() {
 func mpage(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.html", gin.H{
 		"title": "Главная страница",
-		"name": "InvestStarter",
+		"name":  siteName,
 	})
 }
