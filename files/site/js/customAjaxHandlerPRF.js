071045d3c6f244e736fa65d45049782d50d6db1a
miptWW <leskin.in@phystech.edu>
2016-05-21 10:04:24 +0300

    returning back

--- files/site/js/customAjaxHandlerPRF.js
+++ files/site/js/customAjaxHandlerPRF.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $( "#formVK" ).submit(function(event) {
   event.preventDefault();
   
@@ -16,29 +15,8 @@ $( "#formVK" ).submit(function(event) {
 	$(".vkDAE").removeAttr("disabled");
   })
   .fail(function() {
+	alert("Fail");
     $("#loadVK").hide();
 	$(".vkDAE").removeAttr("disabled");
   });
-=======
-$( "#formVK" ).submit(function(event) {
-  event.preventDefault();
-  
-  var $form = $( this ),
-	vkidI = $form.find( "input[name='vkid']" ).val(),
-	urlI = $form.attr( "action" );
-	
-  var posting = $.post( urlI, { vklink: vkidI })
-  .always(function() {
-	$("#loadVK").show();
-	$(".vkDAE").attr("disabled");
-  })
-  .done(function() {
-    $("#loadVK").hide();
-	$(".vkDAE").removeAttr("disabled");
-  })
-  .fail(function() {
-    $("#loadVK").hide();
-	$(".vkDAE").removeAttr("disabled");
-  });
->>>>>>> ac09b82467015a144e1e099f0ca6c1b1a1d02bb9
 });
